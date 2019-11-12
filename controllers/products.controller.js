a86690ab4f48727860d016bc0b482f88b12e297e
Sigurros Inga Helgadottir <6140228@RTS-365.dk>
2019-11-12 13:20:56 +0100

    en anden måde at skrive denne crud på - har kun create tilbage

--- controllers/products.controller.js
+++ controllers/products.controller.js
@@ -1,5 +1,6 @@
 const ProductRef = require("../models/product.model");
 
+const { log } = require("../middleware/logger");
 /* -------------------------------CRUD------------------------------- */
 
 /* -------------------------------Create------------------------------- */
@@ -14,45 +15,112 @@ exports.createProduct = function(req, res){
     .catch(error => res.json(error));
 };
 
+/* -------------------------------Create again------------------------------- */
+
 /* -------------------------------Read------------------------------- */
-exports.getAllProducts = function(req, res){
-    ProductRef.get().then(docs =>{
+// exports.getAllProducts = function(req, res){
+//     ProductRef.get().then(docs =>{
+//         const results = [] ;
+//         docs.forEach(doc => results.push(doc.data()))
+//         res.json(results);
+//     });
+// };
+
+// exports.getSingleProduct = function(req, res){
+//     ProductRef.where("sku", "==", req.params.sku).get()
+//         .then(docs => {
+//             docs.forEach(doc => res.json(doc.data()))
+//         });
+// };
+
+/* -------------------------------Read again------------------------------- */
+exports.getAllProducts = async function(req, res){
+    try {
+        const docs = await ProductRef
+        .get();
         const results = [] ;
         docs.forEach(doc => results.push(doc.data()))
         res.json(results);
-    });
+    } catch (error) {
+        log.error(error.stack);
+        res.status(500).end();      
+    }
 };
 
-exports.getSingleProduct = function(req, res){
-    ProductRef.where("sku", "==", req.params.sku).get()
-        .then(docs => {
-            docs.forEach(doc => res.json(doc.data()))
-        });
-};
+exports.getSingleProduct = async function(req, res){
+    try{
+        const docs = await ProductRef
+        .where("sku", "==", req.params.sku)
+        .limit(1)
+        .get();
+        docs.forEach(doc => res.json(doc.data()));
+    } catch (error){
+        log.error(error.stack);
+        res.status(500).end();
+    }
+}
 
 /* -------------------------------Update------------------------------- */
-exports.patchProduct = function(req, res){
+// exports.patchProduct = function(req, res){
+//     if(req.fields.price){
+//        req.fields.price = parseFloat(req.fields.price); 
+//     }
+//     if(req.fields.weight){
+//         req.fields.weight = parseFloat(req.fields.weight);
+//     }
+    
+//     ProductRef.where("sku", "==", req.params.sku).get()
+//         .then(docs =>{
+//             docs.forEach(doc => doc.ref.update({ ...req.fields }).get()
+//                 .then(doc => res.json(doc.data()))
+//             );
+//         })
+// };
+/* -------------------------------Update again------------------------------- */
+exports.patchProduct = async function(req, res){
     if(req.fields.price){
        req.fields.price = parseFloat(req.fields.price); 
     }
     if(req.fields.weight){
         req.fields.weight = parseFloat(req.fields.weight);
     }
-    
-    ProductRef.where("sku", "==", req.params.sku).get()
-        .then(docs =>{
-            docs.forEach(doc => doc.ref.update({ ...req.fields }).get()
-                .then(doc => res.json(doc.data()))
-            );
-        })
+    try {
+        const docs = await ProductRef
+            .where("sku", "==", req.params.sku)
+            .limit(1)
+            .get()
+        
+            docs.forEach(async doc => {
+                doc.ref.update({ ...req.fields });
+                const result = await doc.ref.get();
+                res.json(result.data())
+            });
+    } catch (error) {
+        log.error(error.stack);
+        res.status(500).end();
+    }
 };
 
 /* -------------------------------Delete------------------------------- */
-exports.deleteProduct = function(req, res){
-    ProductRef.where("sku", "==", req.params.sku).get()
-        .then(docs => {
-            docs.forEach(doc => doc.ref.delete());
-        })
-        .catch(err => res.status(500).json({message: err}));
-    res.status(204).end();
+// exports.deleteProduct = function(req, res){
+//     ProductRef.where("sku", "==", req.params.sku).get()
+//         .then(docs => {
+//             docs.forEach(doc => doc.ref.delete());
+//         })
+//         .catch(err => res.status(500).json({message: err}));
+//             res.status(204).end();
+// };
+/* -------------------------------Delete again------------------------------- */
+exports.deleteProduct = async function(req, res){
+    try{
+        const docs = await ProductRef
+            .where("sku", "==", req.params.sku)
+            .limit(1)
+            .get();
+        docs.forEach(doc => doc.ref.delete());
+        res.status(204).end()
+    }catch (error){
+        log.error(error.stack);
+        res.status(204).end();
+    }
 };
